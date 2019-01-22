326b625af6020f39a5e6fb79078fcb10697b61d1
RRRLuxoft <rrr.lands@gmail.com>
2019-01-22 16:28:44 +0100

    TurorArrayBuffers

--- src/module2_arrays/TutorTransform.scala
+++ src/module2_arrays/TutorTransform.scala
@@ -6,7 +6,7 @@ object TutorTransform extends App {
   val array = Array[Int](2, 3, 5, 7, 11)
 
   // TODO use for .. until to print values with indexes, like "0:2 1:3 2:5 3:7 4:11"
-  for (i <- 0 until array.length) print(s"$i:${array(i)} ")
+  for (i <- 0 until array.length) print(s"$i:${array(i)}")
 
   // TODO use for ... yield to multiply every element by 2 and return the result
   val result = for (e <- array) yield e * 2 // should be Array(4, 6, 10, 14, 22)
@@ -19,7 +19,7 @@ object TutorTransform extends App {
   array.filter(_ % 2 == 0).map(_ * 2)
 
   // TODO calculate amount of elements bigger than 5 (should be 2)
-  println(array.count(_ > 5))
+  println(s"\nCount ${array.count(_ > 5)}")
 
   // TODO create multidimensional array matrix of type Double,
   // TODO with 3 rows and 4 columns
