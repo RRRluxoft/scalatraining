a4a05b3b85666bd779a336b80c27605deeae74e7
RRRLuxoft <rrr.lands@gmail.com>
2019-01-23 16:14:27 +0100

    TutorTraitAbstractField

--- src/module4_traits/TutorTraitAbstractField.scala
+++ src/module4_traits/TutorTraitAbstractField.scala
@@ -1,23 +1,31 @@
 package module4_traits
 
 object TutorTraitAbstractField extends App {
+
   // abstract field in a trait
   trait Logged {
     def log(msg: String) {}
   }
-  class Account { var balance: Double = 0 }
+
+  class Account {
+    var balance: Double = 0
+  }
 
   // TODO define maxLength as abstract field
   trait ShortLogger extends Logged {
+    val maxLength: Int
+
     override def log(msg: String) {
       //TODO uncomment
-      //super.log(if (msg.length <= maxLength) msg
-      //else msg.substring(0, maxLength - 3) + "...")
+      super.log(if (msg.length <= maxLength) msg
+      else msg.substring(0, maxLength - 3) + "...")
     }
   }
 
   trait ConsoleLogger extends Logged {
-    override def log(msg: String) { println(msg) }
+    override def log(msg: String) {
+      println(msg)
+    }
   }
 
   // TODO define maxLength value in the class
@@ -26,10 +34,13 @@ object TutorTraitAbstractField extends App {
     def withdraw(amount: Double) {
       if (amount > balance) log("Insufficient funds")
     }
+
+    val maxLength: Int = 15
   }
 
   // TODO define maxLength value for the object
   val logger = new ConsoleLogger with ShortLogger {
+    override val maxLength: Int = 20
   }
 
 
