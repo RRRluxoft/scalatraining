c3697c19941308c8bfd797640de9349de8ea0444
RRRLuxoft <rrr.lands@gmail.com>
2019-01-23 11:23:29 +0100

    Constructor s

--- src/module3_classes/TutorAuxiliaryConstructor.scala
+++ src/module3_classes/TutorAuxiliaryConstructor.scala
@@ -4,8 +4,26 @@ object TutorAuxiliaryConstructor {
 
   // TODO create class Person with private fields name and age with default values
   // TODO create auxiliary constructors this(name) and this(name,age)
+  class Person {
+    private var name = ""
+    private var age = 0
+
+    def this(name: String) {
+      this()
+      this.name = name
+    }
+
+    def this(name: String, age: Int) {
+      this(name)
+      this.age = age
+    }
+  }
 
   // TODO create Person using constructor with no params, with name parameter
   // TODO and with name and age
+
+  val p1 = new Person
+  val p2 = new Person("Sting")
+  val p3 = new Person("Sting", 42)
 
 }
--- src/module3_classes/TutorClassConstructors.scala
+++ src/module3_classes/TutorClassConstructors.scala
@@ -4,18 +4,35 @@ package module3_classes
 object TutorClassConstructors extends App {
 
   // TODO create simple class First and create instance of the class
+  class First
+  val c1 = new First()
 
   // TODO create class Second and instance, print hello from constructor body
+  class Second {
+    println("Hello")
+  }
+  val c2 = new Second
 
   // TODO create class Third with a class parameter
   // TODO create var and val properties in class
   // TODO set its default values to this parameter
+  class  Third(_a: Int) {
+    val a = _a
+    var b = _a
+  }
 
   // TODO create class instance, access property defined as val
   // TODO change and access property defined as var
 
   // TODO create class Forth with constructor parameters a and b
   // TODO which will be read-write properties
+  class Forth(var a:Int, var b:Int=2) { // b has default value
+    override def toString = a+":"+b // overriding
+  }
+  val b = new Forth(1,2)
+  println(b) // 1:2
+  val c = new Forth(3)
+  println(c) // 3:2
 
   // TODO for parameter b set the default value
 
