71ee386a51a6b2cf609ea608a40fbd52d2c2372d
RRRLuxoft <rrr.lands@gmail.com>
2019-01-23 15:27:44 +0100

    TutorTraits

--- src/module4_traits/TutorTraits.scala
+++ src/module4_traits/TutorTraits.scala
@@ -5,14 +5,32 @@ object TutorTraits extends App {
   // trait as interface:
   // create trait Logger with method log(msg:String)
   // implement it in a class
+  trait Logger {
+    def log(msd: String)
+  }
+
+  class ConLogger extends Logger {
+    // Use extends, not implements
+    def log(msg: String) {
+      println(msg)
+    } // No override
+  }
 
   // trait with concrete implementation
   // TODO create trait ConsoleLogger0 with already implemented log(msg:String)
+  trait ConsoleLogger0 {
+    def log(msg: String) = {
+      println(msg)
+    }
+  }
 
   class Account {
     var balance: Double = 0
   }
-  trait Logged { def log(msg: String) {} }
+
+  trait Logged {
+    def log(msg: String) {}
+  }
 
   class SavingsAccount extends Account with Logged {
     def withdraw(amount: Double) {
@@ -31,6 +49,7 @@ object TutorTraits extends App {
   // TODO create acct variable as SavingsAccount instance
   // TODO with ConsoleLogger mixed in
   // TODO do withdraw to check logging
+  val acct = new SavingsAccount with ConsoleLogger
 
   trait FileLogger extends Logged {
     override def log(msg: String) {
@@ -41,6 +60,6 @@ object TutorTraits extends App {
   // TODO create acct variable as SavingsAccount instance
   // TODO with FileLogger mixed in
   // TODO do withdraw to check logging
-
+  val acct2 = new SavingsAccount() with FileLogger
 
 }
