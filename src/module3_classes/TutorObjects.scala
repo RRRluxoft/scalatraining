92b944e4e302ba922262ca404eba844e64a85b27
RRRLuxoft <rrr.lands@gmail.com>
2019-01-23 13:38:52 +0100

    TutorClasses

--- src/module3_classes/TutorObjects.scala
+++ src/module3_classes/TutorObjects.scala
@@ -5,6 +5,14 @@ object TutorObjects extends App {
   // TODO create Accounts object which will allow to generate
   // TODO the unique number of account using
   // TODO Accounts.newUniqueNumber
+  object Accounts {
+    private var lastNumber = 0
+
+    def newUniqueNumber() = {
+      lastNumber += 1;
+      lastNumber
+    }
+  }
 
   // TODO let we have class Account
   // TODO define apply() method which will allow to create
@@ -12,8 +20,14 @@ object TutorObjects extends App {
   // val account = Account(1000)
   // 1000 is the initialBalance value
   // TODO generate id using Accounts.newUniqueNumber
-  class Account private (val id: Int, initialBalance: Double) {
+  class Account private(val id: Int, initialBalance: Double) {
     private var balance = initialBalance
   }
+
+  object Account {
+    def apply(initNumber: Int): Account = new Account(Accounts.newUniqueNumber(), initNumber)
+  }
+
+  val account = Account(10000)
 
 }
