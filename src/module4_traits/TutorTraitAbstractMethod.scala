d2b981e45305d39b39706ec1d3fe4a49036aa854
RRRLuxoft <rrr.lands@gmail.com>
2019-01-23 15:50:56 +0100

    TutorTraitAbstractMethod

--- src/module4_traits/TutorTraitAbstractMethod.scala
+++ src/module4_traits/TutorTraitAbstractMethod.scala
@@ -2,6 +2,7 @@ package module4_traits
 
 // abstract method to access super
 object TutorTraitAbstractMethod extends App {
+
   trait Logger {
     def log(msg: String) // log method is abstract
   }
@@ -10,13 +11,20 @@ object TutorTraitAbstractMethod extends App {
   // it should do
   // super.log(new java.util.Date()+" "+msg)
   trait TimestampLogger extends Logger {
+    abstract override def log(msg: String) = {
+      super.log(new java.util.Date() + " " + msg)
+    }
   }
 
   class PrintLogger extends Logger {
-    def log(msg: String) { println(msg) }
+    def log(msg: String) {
+      println(msg)
+    }
   }
 
   // TODO define variable as PrintLogger with TimestampLogger
   // TODO use log() - it should print message with the date
+  val log = new PrintLogger with TimestampLogger
+  log.log("ah - ah")
 
 }
