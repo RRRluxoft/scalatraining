b9b9a6348f96d8ae6fdfbb55f7627ccf6e9c3e78
RRRLuxoft <rrr.lands@gmail.com>
2019-01-24 16:04:12 +0100

    TutorOperations

--- src/module6_collections/TutorOperations.scala
+++ src/module6_collections/TutorOperations.scala
@@ -4,11 +4,14 @@ import scala.collection.mutable.ArrayBuffer
 
 object TutorOperations extends App {
 
-  val vec = Vector(1,2,3)
+  var vec = Vector(1,2,3)
   // Seq has :+ operator to add elements
   // TODO create vector1 by appending 5 to vec
+  val vector1 = vec :+ 5
 
   // TODO create vector2 by prepending 1 to vec
+  var vector2 = 1 +: vec
+  vector2 :+= 7
 
   var vector3 = Vector(1, 2, 3)
   vector3 :+= 5 // += does not work since vectors don't have a + operator
@@ -17,18 +20,25 @@ object TutorOperations extends App {
 
   // ++ is available for Iterable
   // TODO use ++ to concatenate vector1 and vector2
+  println(vector2 ++ vector3)
 
   // operations for mutable collections
   val numbers = ArrayBuffer(1, 2, 3)
   // TODO append 5 to numbers
+  numbers += 5
 
   var numbers2 = Set(1, 2, 3)
   // TODO append 5 to numbers2, print the result
   // TODO remove 2 from numbers2, print the result
   // TODO append (3,4) tuple to numbers2, print the result
+  numbers2 += 5
+  numbers2 -= 2
+  numbers2 += (3,4)
 
   // TODO append numbers2 to numbers by using ++=
+  numbers ++= numbers2
 
   // TODO remove all elements in numbers2 from numbers, use --=
+  numbers --= numbers2
 
 }
