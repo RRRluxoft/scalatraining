bfeba235fdab62d28ac76cd2368dc867645c9215
RRRLuxoft <rrr.lands@gmail.com>
2019-01-24 15:26:31 +0100

    TutorSets

--- src/module6_collections/TutorSets.scala
+++ src/module6_collections/TutorSets.scala
@@ -6,24 +6,33 @@ object TutorSets extends App {
   var s = Set(2, 0, 1)
   // Set is a collection of distinct elements
   // TODO create set by appending 1 to s, use +
+  println(s + 1)
+
 
   // set do not retain order (unlike lists or ArrayBuffer/Vector)
-  val set2 = Set(1,2,3,4,5,6)
+  val set2 = Set(1, 2, 3, 4, 5, 6)
   // TODO use foreach to print every element of set, see the result
+  set2.foreach(e => println(e.toString.mkString(", ")))
 
   // linked hash set remembers the order in which elements were inserted
-  val linkedSet = mutable.LinkedHashSet(6,5,4,3,2,1)
+  val linkedSet = mutable.LinkedHashSet(6, 5, 4, 3, 2, 1)
   // TODO print linkedSet, is it ordered? how?
+  println(linkedSet)
 
   // if you want to keep elements in sorted order, use SortedSet
-  val sortedSet = mutable.SortedSet(6,5,4,3,2,1)
+  val sortedSet = mutable.SortedSet(6, 5, 4, 3, 2, 1)
   // TODO print sortedSet, is it ordered?
+  println(sortedSet)
 
   // contains and subsetOf methods
   val digits = Set(1, 7, 2, 9)
   // TODO check if 0 is element of digits (use contains)
   // TODO check if 1 is element of digits (use contains)
   // TODO check if Set(1,2) is subset of digits (use subsetOf)
+  println(digits.contains(0))
+  println(digits.contains(1))
+  println(Set(1, 2) subsetOf digits)
+
 
   // union (or ++) , intersect (or &), and diff (or --) methods
   val primes = Set(2, 3, 5, 7)
@@ -31,5 +40,8 @@ object TutorSets extends App {
   // TODO use & to get intersection of digits and primes
   // TODO use -- to get diff of digits and primes
 
+  println(primes ++ digits)
+  println(primes & digits)
+  println(primes -- digits)
 
 }
