d32960029b412372702a30547fc68febee3c6a0a
RRRLuxoft <rrr.lands@gmail.com>
2019-01-25 11:33:03 +0100

    TutorPatternMatching

--- src/module2_arrays/TutorAlgorithms.scala
+++ src/module2_arrays/TutorAlgorithms.scala
@@ -1,16 +1,27 @@
 package module2_arrays
 
+import scala.collection.convert.ImplicitConversions.`seq AsJavaList`
+
 
 object TutorAlgorithms extends App {
+
   // TODO create array having values 1,2,3,4 and calculate its sum
+  val arr = Array[Int](1, 2, 3, 4)
 
   // TODO create ArrayBuffer with values "one", "two", "three" and get its maximal value
+  import scala.collection.mutable.ArrayBuffer
+
+  val buf = ArrayBuffer[String]("one", "two", "three")
+  buf.max
 
   // TODO create ArrayBuffer b having values 2,6,9,1
+  val b = ArrayBuffer(2, 6, 9, 1)
 
   // TODO sort it in ascending order and print
+  println(b.sorted.mkString("<", ",", ">"))
 
   // TODO sort it in descending order and print (use sortBy)
+
 
   // TODO create Array a with values 1,7,2
 
--- src/module7_patternmatching/TutorPatternMatching.scala
+++ src/module7_patternmatching/TutorPatternMatching.scala
@@ -8,18 +8,34 @@ object TutorPatternMatching extends App {
 
   // TODO use pattern matching to set sign to 1 in case if ch is '+',
   // TODO to -1 if ch is '-' and to 0 in all other cases
+  ch match {
+    case '+' => sign = 1
+    case '-' => sign = -1
+    case _ => 0
+  }
 
   // using match as expression
   // TODO assign result of match directly to sign variable,
   // TODO like sign = ch match {...}
+  sign = ch match {
+    case '-' => -1
+    case '+' => 1
+    case _ => 0
+  }
 
   // guards
   // TODO use pattern matching to find if rank of developer
   // TODO it should be "junior" if developer is younger than 30,
   // TODO "middle" if he's 30, and "senior" if he's older
   // TODO in case if it is digit, set digit to Character.digit(ch, 10)
-  var age = 30
-  val rank = ""
+  val age = 30
+  val rank = age match {
+    case _ if age < 30 => "junior"
+    case _ if age == 30 => "middle"
+    case _ if age > 30 => "senior"
+    case _ => "undefined"
+  }
+
   println(s"Developer is $age years old and he's $rank")
   // should print "Developer is 30 years old and he's middle"
 
@@ -46,7 +62,13 @@ object TutorPatternMatching extends App {
   // TODO set to Integer.parseInt(obj) if it was String,
   // TODO set res to Int.MaxValue if it was BigInt,
   // TODO and set to 0 otherwise
-  val res = 0
+
+  val res = obj match {
+    case n: Int => n
+    case s:String => Integer.parseInt(s)
+    case _: BigInt => Int.MinValue
+    case _ => 0
+  }
   println(res) // should print 15
 
   // matching Array
@@ -58,7 +80,12 @@ object TutorPatternMatching extends App {
   // TODO set to "0 ..." in case if Array has 0 as first element
   // TODO and more than 1 of other elements
   // TODO otherwise, set to "something else"
-  val res0 = ""
+  val res0 = arr match {
+    case Array(0) => "0"
+    case Array(x ,y) => x + " " + y
+    case Array(0, _*) => "0 ..."
+    case _ => "smth else"
+  }
   println(res0) // should print "1 2"
 
   // matching List
@@ -69,7 +96,12 @@ object TutorPatternMatching extends App {
   // TODO set to "0 ..." in case if List has 0 as first element
   // TODO and more than 1 of other elements
   // TODO otherwise, set to "something else"
-  val res1 = 0
+  val res1 = lst match {
+    case 0 :: Nil => "0"
+    case a :: b :: Nil => a + " " + b
+    case 0 :: tail => "0 ..."
+    case _ => "something else"
+  }
   println(res1) // should print "1 2"
 
   // matching tuple
@@ -78,7 +110,12 @@ object TutorPatternMatching extends App {
   // TODO to "very good" if grades are 5 and 4
   // TODO to "unsatisfactory" if any grade is 2
   // TODO and "ok" otherwise
-  val result = ""
+  val result = grades match {
+    case (5 , 5) => "perfect"
+    case (4 , 5) | (4, 5) => "very good"
+    case (2 , _) | (_, 2) => "unsatisfactory"
+    case _ => "ok"
+  }
   println(result) // should be "very good"
 
   // regular expressions
@@ -86,5 +123,10 @@ object TutorPatternMatching extends App {
   // TODO should match "99 bottles" to pattern(num, item)
   // TODO and print result like "we have 99 of bottles"
   // TODO syntax: case pattern(num, item)
+  val res3 = "99 bottles" match {
+    case pattern(n, i) => s"we have ${n} of ${i}"
+  }
+
+  println(res3)
 
 }
