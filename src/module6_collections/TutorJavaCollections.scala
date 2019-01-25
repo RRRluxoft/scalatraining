f47327fd7556e9b5dc06b3980c6afd61d6ac06fd
RRRLuxoft <rrr.lands@gmail.com>
2019-01-25 10:09:38 +0100

    TutorJavaCollections

--- src/module6_collections/TutorJavaCollections.scala
+++ src/module6_collections/TutorJavaCollections.scala
@@ -3,40 +3,47 @@ package module6_collections
 import scala.collection.mutable.ArrayBuffer
 
 object TutorJavaCollections extends App {
+
   import scala.collection.JavaConversions._
 
   // conversions for Scala Map<=>Java Map
-  val jmap: java.util.Map[Int,Int] = Map(1->2) // implicit conversion
-  println( jmap ) // {1=2}
-  val scalaMap: scala.collection.Map[Int,Int] = jmap // implicit conversion
-  println( scalaMap ) // Map(1 -> 2)
-  val scalaMap2 = mapAsScalaMap( jmap ) // explicit conversion
-  println( scalaMap2 ) // Map(1 -> 2)
+  val jmap: java.util.Map[Int, Int] = Map(1 -> 2) // implicit conversion
+  println(jmap) // {1=2}
+  val scalaMap: scala.collection.Map[Int, Int] = jmap // implicit conversion
+  println(scalaMap) // Map(1 -> 2)
+  val scalaMap2 = mapAsScalaMap(jmap) // explicit conversion
+  println(scalaMap2) // Map(1 -> 2)
   val javaMap = mapAsJavaMap(scalaMap) // explicit conversion
-  println( javaMap ) // {1=2}
+  println(javaMap) // {1=2}
   //javaMap.put(2,3) // exception: UnsupportedOperationException
   // to work with the mutable map, use:
-  val mutableMap = new scala.collection.mutable.HashMap[String,String]
+  val mutableMap = new scala.collection.mutable.HashMap[String, String]
   val javaMapMutable = mutableMapAsJavaMap(mutableMap)
 
   // conversions for Buffer<=>List
   val buf = ArrayBuffer("one", "two", "three")
   // TODO create list of type java.util.ArrayList and
   // TODO fill it with buf by passing buf to constructor
+  val list = new java.util.ArrayList(buf)
   // explicit conversion to Scala Buffer
   // TODO use explicit conversion asScalaBuffer to get Buffer from list
+  val buf2 = asScalaBuffer(list)
+  println(buf2)
   // implicit conversion from ArrayList to Buffer
   // TODO define buf3 as mutable Buffer, and assign list to it
+  val buf3: scala.collection.mutable.Buffer[String] = list
   // implicit conversion from Buffer to Java List
   // TODO define list as java.util.List and assign buf3 to it
+  val list2: java.util.List[String] = buf3
   // explicit conversion to Java List
   // TODO use bufferAsJavaList to explicitly convert buf3 to Java list
+  val list3 = bufferAsJavaList(buf3)
 
   // conversions for Scala Set<=>Java Set
   val set = new java.util.HashSet[String]()
   set.add("Hi")
   val scalaSet = asScalaSet(set) // explicit conversion
-  println( scalaSet ) // Set(Hi)
+  println(scalaSet) // Set(Hi)
   // implicit conversion from Java Set to Scala Set
   val scalaSet2: scala.collection.mutable.Set[String] = set
   // implicit conversion from Scala Set to Java Set
@@ -45,8 +52,10 @@ object TutorJavaCollections extends App {
   // implicit conversion for properties
   val props: scala.collection.mutable.Map[String, String] =
     System.getProperties()
+  println("User country -> " + props("user.country"))
+  println(props)
 
   // conversion of Java List to Seq
-  val javaList = seqAsJavaList(Seq(1,2,3))
+  val javaList = seqAsJavaList(Seq(1, 2, 3))
 
 }
