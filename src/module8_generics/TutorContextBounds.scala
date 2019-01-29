d8c25d965929ddeaa985d30d4ed1684a054399d8
RRRLuxoft <rrr.lands@gmail.com>
2019-01-29 17:59:20 +0100

    TutorContextBounds

--- src/module8_generics/TutorContextBounds.scala
+++ src/module8_generics/TutorContextBounds.scala
@@ -1,6 +1,7 @@
 package module8_generics
 
 object TutorContextBounds extends App {
+
   // context bound
   // it's improved view bound introduces in Scala 2.8.0
 
@@ -9,33 +10,42 @@ object TutorContextBounds extends App {
   // there is an implicit value of type B[A] available
 
   trait Maxable[T] {
-    def max(a:T, b:T):T
+    def max(a: T, b: T): T
   }
-  class Person(val name: String, val age:Int)
+
+  class Person(val name: String, val age: Int)
 
   class PersonMaxable extends Maxable[Person] {
-    def max(x:Person,y:Person):Person = if (x.age>y.age) x else y
+    def max(x: Person, y: Person): Person = if (x.age > y.age) x else y
   }
+
   implicit object Person extends PersonMaxable
+
   val person1 = new Person("John", 20)
   val person2 = new Person("George", 30)
-  println( Person.max(person1, person2).name ) // George
+  println(Person.max(person1, person2).name) // George
 
   // TODO implement Pair with first and second parameters of generic type
   // TODO for which Maxable[T] is defined
 
   // TODO define method max which takes Maxable[T] as implicit parameter
   // TODO and use it to find max of first and second
+  class Pair[T: Maxable](val first: T, val second: T) {
+    def max(implicit maxable: Maxable[T]) =
+      maxable.max(first, second)
+  }
 
-  /* TODO uncomment this
+  /* TODO uncomment this */
   val pair = new Pair(person1, person2)
-  println( pair.max.name )
-  */
+  println(pair.max.name)
 
   // example with Ordering trait (Ordering is already implemented in API)
-  class Pair2[T : Ordering](val first: T, val second: T) {
+  class Pair2[T: Ordering](val first: T, val second: T) {
     def smaller(implicit ord: Ordering[T]) =
       if (ord.compare(first, second) < 0) first else second
   }
+
+  val p = new Pair2[Int](2, 3)
+  println(p.smaller)
 
 }
