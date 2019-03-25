6147696e71b023b6edfe130a34409cc746a232b3
RRRLuxoft <rrr.lands@gmail.com>
2019-03-25 20:32:56 +0100

    gcd

--- src/module1_intro/HelloWorld2.scala
+++ src/module1_intro/HelloWorld2.scala
@@ -2,4 +2,23 @@ package module1_intro
 
 object HelloWorld2 extends App {
   println("Hello World")
+
+  def gcdLoop(x: Long, y: Long): Long = {
+    var a = x
+    var b = y
+    while (a != 0) {
+      val tmp = a
+      a = b % a
+      b = tmp
+    }
+    b
+  }
+
+  def gcd(a: Long, b: Long): Long = {
+    if (b == 0) a else gcd(b, a % b)
+  }
+
+  println(gcdLoop(15, 55))
+  println(gcd(55, 15))
+  println(s"are equal? ${gcdLoop(15, 55).toLong == gcd(55, 15).toLong}")
 }
